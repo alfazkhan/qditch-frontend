b555fae4f69095070b2dcae6133aecd8e2d6f9fd
alfazkhan <khanalfaaz14@gmail.com>
2020-06-25 18:29:23 +0530

    Bug fix

--- src/Axios.js
+++ src/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-  baseURL: `https://13.234.31.245:8000/api/`,
+  baseURL: `http://13.234.31.245:8000/api/`,
   headers:{
     'Access-Control-Allow-Origin' : '*',
     'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
